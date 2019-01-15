8b68c3c3411d7e9a409fa54d181c0f2ce4435525
vandrep <andrepinedacunha@gmail.com>
2019-01-15 08:29:27 -0200

    primeiro commit

---
+++ src/app/rotas/rotas.js
@@ -0,0 +1,54 @@
+const LivroDao = require('../infra/livro_dao');
+const db = require('../../config/database');
+
+module.exports = app => {
+
+    app.get('/', (req,resp) => {
+        resp.send(
+            `
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1>Casa do Código</h1>
+                    </body
+                </html>
+            `
+        );
+    });
+
+    app.get('/livros', (req,resp) => {
+
+        const livroDao = new LivroDao(db);
+        livroDao.lista()
+            .then(livros => 
+                resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: livros
+                }
+            ))
+            .catch(erro => console.log(erro));
+    });
+
+    app.get('/livros/form', (req, resp) => {
+        resp.marko(require('../views/livros/form/form.marko'));
+    });
+
+    app.post('/livros', (req, resp) => {
+        console.log(req.body);        
+
+        const livroDao = new LivroDao(db);
+
+        livroDao.adiciona(req.body)
+            .then(livros => 
+                resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: livros
+                }
+            ))
+            .catch(erro => console.log(erro));
+    })
+};
